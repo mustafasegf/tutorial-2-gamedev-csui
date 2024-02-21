1a5239d37556f049611074ee7365fbc22c0b56f2
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-02-21 14:37:50 +0700

    feat: make an actual level fr fr

--- scenes/BlueShip.gd
+++ scenes/BlueShip.gd
@@ -7,6 +7,10 @@ var direction := Vector2.ZERO
 var desired_velocity := Vector2.ZERO
 var steering_vector := Vector2.ZERO
 var velocity := Vector2.ZERO
+var screensize
+
+func _ready():
+		screensize = get_viewport().get_visible_rect().size
 
 func _physics_process(delta: float) -> void:
 		direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -23,3 +27,16 @@ func _physics_process(delta: float) -> void:
 		# Rotate towards movement direction
 		if velocity.length() > 0:
 				rotation = velocity.angle() + deg2rad(90)
+
+		# Screen wrap logic
+		var new_position = position
+		if position.x < 0:
+				new_position.x = screensize.x
+		elif position.x > screensize.x:
+				new_position.x = 0
+		if position.y < 0:
+				new_position.y = screensize.y
+		elif position.y > screensize.y:
+				new_position.y = 0
+		
+		position = new_position
